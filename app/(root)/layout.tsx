750a28ee93203d80d4379ec24cfc828b33828397
NickRaise <nikhilnainwaya17@gmail.com>
2025-04-01 12:41:41 +0530

    created the basic skeleton of homepage

--- app/(root)/layout.tsx
+++ app/(root)/layout.tsx
@@ -1,11 +1,25 @@
-import React, { ReactNode } from 'react'
+import Image from "next/image";
+import Link from "next/link";
+import React, { ReactNode } from "react";
 
-const RootLayout = ({children}: {children: ReactNode}) => {
+const RootLayout = ({ children }: { children: ReactNode }) => {
   return (
-    <div>
-        {children}
+    <div className="root-layout">
+      <nav>
+        <Link href="/" className="flex items-center gap-2">
+          <Image
+            src="/logo.svg"
+            alt="Logo"
+            height={38}
+            width={38}
+            style={{ width: "auto", height: "auto" }}
+          />
+          <h2 className="text-primary-100">Interview Guru</h2>
+        </Link>
+      </nav>
+      {children}
     </div>
-  )
-}
+  );
+};
 
-export default RootLayout
+export default RootLayout;
